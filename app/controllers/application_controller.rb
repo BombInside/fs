af5e78d55594ac60a36bae3bc570561705e76167
BombInside <xtc.inside@gmail.com>
2015-03-03 09:19:31 +0000

    auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
    protect_from_forgery with: :exception
 
+   helper_method :current_user
+   
    def host
-   	@host = request.host_with_port 	
+   	@host = request.host_with_port
    end
 
   private
@@ -16,6 +18,11 @@ class ApplicationController < ActionController::Base
   	render template: "error/404", status: 404
   end
 
+  def current_user
+    User.find_by id: session[:user_id] if session[:user_id]
 
- 
+  end
+
+
+
 end
